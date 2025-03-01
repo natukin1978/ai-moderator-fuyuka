8a161c6d4ab26dff2561807ffad9e7e42d5942a9
natukin1978 <natukin1978@hotmail.com>
2025-03-02 01:11:13 +0900

    I replaced it while looking at the update manual.
    https://ai.google.dev/gemini-api/docs/migrate?hl=ja

--- genai_chat.py
+++ genai_chat.py
@@ -3,12 +3,8 @@ import logging
 import os
 import pickle
 
-import google.generativeai as genai
-from google.generativeai.types import (
-    HarmBlockThreshold,
-    HarmCategory,
-    StopCandidateException,
-)
+from google import genai
+from google.genai import chats, errors, types
 
 import global_value as g
 from cache_helper import get_cache_filepath
@@ -19,31 +15,46 @@ logger = logging.getLogger(__name__)
 class GenAIChat:
     FILENAME_CHAT_HISTORY = get_cache_filepath(f"{g.app_name}_gen_ai_chat_history.pkl")
 
-    GENAI_SAFETY_SETTINGS = {
+    GENAI_SAFETY_SETTINGS = [
         # ハラスメントは中程度を許容する
-        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        types.SafetySetting(
+            category=types.HarmCategory.HARM_CATEGORY_HARASSMENT,
+            threshold=types.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        ),
         # ヘイトスピーチは厳しく制限する
-        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        types.SafetySetting(
+            category=types.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            threshold=types.HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        ),
         # セクシャルな内容を多少は許容する
-        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        types.SafetySetting(
+            category=types.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold=types.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        ),
         # ゲーム向けなので、危険に分類されるコンテンツを許容できる
-        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-    }
+        types.SafetySetting(
+            category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold=types.HarmBlockThreshold.BLOCK_NONE,
+        ),
+    ]
 
     def __init__(self):
         conf_g = g.config["google"]
-        genai.configure(api_key=conf_g["geminiApiKey"])
-        self.genaiModel = genai.GenerativeModel(
-            model_name=conf_g["modelName"],
-            safety_settings=self.GENAI_SAFETY_SETTINGS,
-            system_instruction=g.BASE_PROMPT,
-        )
+        self.client = genai.Client(api_key=conf_g["geminiApiKey"])
         self.chat_history = None
         self.genaiChat = None
 
-    def get_chat(self) -> genai.ChatSession:
+    def get_chat(self) -> chats.AsyncChat:
         if not self.genaiChat:
-            self.genaiChat = self.genaiModel.start_chat(history=self.chat_history)
+            conf_g = g.config["google"]
+            self.genaiChat = self.client.aio.chats.create(
+                model=conf_g["modelName"],
+                config=types.GenerateContentConfig(
+                    system_instruction=g.BASE_PROMPT,
+                    safety_settings=self.GENAI_SAFETY_SETTINGS,
+                ),
+                history=self.chat_history,
+            )
         return self.genaiChat
 
     def reset_chat_history(self) -> None:
@@ -60,18 +71,22 @@ class GenAIChat:
 
     def save_chat_history(self) -> None:
         with open(self.FILENAME_CHAT_HISTORY, "wb") as f:
-            pickle.dump(self.get_chat().history, f)
+            pickle.dump(self.get_chat()._curated_history, f)
 
     async def send_message(self, message: str) -> str:
         try:
             logger.debug(message)
             chat_session = self.get_chat()
-            response = await chat_session.send_message_async(message)
-            response_text = response.text.rstrip()
+            response = await chat_session.send_message(message)
+            response_text = response.text
+            if response_text:
+                response_text = response_text.rstrip()
+            else:
+                response_text = ""
             logger.debug(response_text)
             self.save_chat_history()
             return response_text
-        except StopCandidateException as e:
+        except errors.APIError as e:
             logger.error(e)
             return g.STOP_CANDIDATE_MESSAGE
         except IndexError as e:
