ba2e8d63cfd7aa34bcf414c55a06fe764ae9a5fa
natukin1978 <natukin1978@hotmail.com>
2026-04-29 20:58:38 +0900

    The helper for the configuration file has been updated.

--- config_helper.py
+++ config_helper.py
@@ -4,7 +4,25 @@ import os
 import global_value as g
 
 
-def readConfig(name: str = "config.json"):
-    json_file_path = os.path.join(g.base_dir, name)
-    with open(json_file_path, "r", encoding="utf-8") as f:
+def read_json(path: str):
+    with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
+
+def write_json(data: any, path: str):
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+def read_config(name: str = "config.json"):
+    if not os.path.isabs(name):
+        name = os.path.join(g.base_dir, name)
+    if not os.path.isfile(name):
+        # 無いならひな形を参照
+        name += ".template"
+        if not os.path.isfile(name):
+            return {}
+    return read_json(name)
+
+def write_config(data: any, name: str = "config.json"):
+    if not os.path.isabs(name):
+        name = os.path.join(g.base_dir, name)
+    write_json(data, name)
