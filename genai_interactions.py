c236bf8aa08a7974f2744c35974e9036c6b28082
natukin1978 <natukin1978@hotmail.com>
2026-06-25 21:22:58 +0900

    Safety settings cannot be configured at this time, so they have been removed.

--- genai_interactions.py
+++ genai_interactions.py
@@ -19,29 +19,6 @@ class GenAIInteractions:
     FILENAME_API_KEY_INDEX = get_cache_filepath(f"{g.app_name}_api_key_index.pkl")
     FILENAME_CHAT_HISTORY = get_cache_filepath(f"{g.app_name}_gen_ai_interactions_history.pkl")
 
-    # GENAI_SAFETY_SETTINGS = [
-    #     # ハラスメントは中程度を許容する
-    #     SafetySetting(
-    #         category=HarmCategory.HARM_CATEGORY_HARASSMENT,
-    #         threshold=HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    #     ),
-    #     # ヘイトスピーチは厳しく制限する
-    #     SafetySetting(
-    #         category=HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    #         threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-    #     ),
-    #     # セクシャルな内容を多少は許容する
-    #     SafetySetting(
-    #         category=HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    #         threshold=HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    #     ),
-    #     # ゲーム向けなので、危険に分類されるコンテンツを許容できる
-    #     SafetySetting(
-    #         category=HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    #         threshold=HarmBlockThreshold.BLOCK_NONE,
-    #     ),
-    # ]
-
     GOOGLE_SEARCH_TOOL = [{"type": "google_search"}]
 
     def __init__(self):
